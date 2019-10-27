ee0651c387e6cba571cbc450dd7db06468fe90e6
ufoo68 <mitarou.yuutarou@gmail.com>
2019-10-27 15:28:51 +0900

    done

--- src/Mypage.js
+++ src/Mypage.js
@@ -2,6 +2,8 @@ import './Mypage.css';
 import React from 'react';
 import QRCode from "qrcode.react"
 import axios from 'axios'
+import img from './aseets/izakaya.png'
+import mp3 from './aseets/maidoari.mp3'
 
 class Mypage extends React.Component {
     constructor(props) {
@@ -12,25 +14,31 @@ class Mypage extends React.Component {
     }
     componentDidMount() {
         const {params} = this.props.match
-        const id = params.id;
-        console.log(id);
-        axios.get(`https://maidoary.herokuapp.com/customers/${id}`)
-        .then((response) => {
-          this.setState({ point: response.data.point });
-        })
+        const id = params.id
         axios.get(`https://maidoary.herokuapp.com/customers/${id}/update`)
+        .then(() => {
+          axios.get(`https://maidoary.herokuapp.com/customers/${id}`)
+          .then((response) => {
+            this.setState({ point: response.data.point })
+          })
+        })
     }
+    startSpeech = () => {
+        const audio = new Audio(mp3)
+        audio.play()
+      }
     render() {
         const {params} = this.props.match
-        const id = params.id;
-        console.log(params.id)
+        const id = params.id
         return (
-            <div class="Mypage">
-                <div class="title">MyCoupon</div>
-                <div class="point">現在{this.state.point+1}ポイント</div>
-                <QRCode value={`https://yolo-b004d.firebaseapp.com/${id}`} />
+            <div className="Mypage">
+                <div className="title" onClick={() =>  this.startSpeech()}>まいどあり～</div>
+                <img src={img} alt="izakaya" className="izakaya"/>
+                <div className="point">現在{this.state.point}ポイント</div>
+                <QRCode value={`${id}`} />
+                <div className="discription">※このページを「ブックマーク」で保存してください</div>
             </div>
-        );
+        )
     }
 }
 
